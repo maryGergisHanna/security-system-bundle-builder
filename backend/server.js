5dcfede55623ecf5bfd6e713cc58ba833596a414
Mary Gergis <mary.gergis@isoft.ae>
2026-07-21 09:53:06 +0300

    update scripts and port of server

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/plans", plansRoutes);
 app.use("/api/accessoirs", accessoirsRoutes);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
